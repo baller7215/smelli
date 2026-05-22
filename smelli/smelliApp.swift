0559cdfa3e05e87f4c1165157fda78c2866ed658
Leo Siu <leonardoy.siu@gmail.com>
2026-05-22 01:47:01 -0700

    Initial Commit

---
+++ smelli/smelliApp.swift
@@ -0,0 +1,17 @@
+//
+//  smelliApp.swift
+//  smelli
+//
+//  Created by Leonardo Siu on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct smelliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
